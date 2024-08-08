63654aba418154cbb0d1c859e5e1980ff83b6e1a
elvrby <alfarabi1074@gmail.com>
2024-08-09 01:21:24 +0700

    .

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,12 +1,22 @@
+"use client"
 import next from "next";
-import React from "react";
+import React, { useState } from "react";
 import Image from "next/image";
 import LoginComponent from "@/components/Mobile/login";
 
 const LoginPage: React.FC = () =>{
+    const [isLoginVisible, setLoginVisible] = useState(true);
+
+    const handleShowLogin = () => {
+      setLoginVisible(true);
+    };
+  
+    const handleCloseLogin = () => {
+      setLoginVisible(false);
+    };
     return(
         <main>
-            <LoginComponent></LoginComponent>
+            {isLoginVisible && <LoginComponent close={handleCloseLogin} />}
         </main>
     )
 }
