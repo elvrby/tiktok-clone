8ccf22a1a4c0c0ff362b5d191e3eac3d5e69cfab
elvrby <alfarabi1074@gmail.com>
2024-08-09 07:25:00 +0700

    .

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -7,33 +7,32 @@ import { useUserSession } from '@/hooks/use-user-session';
 import { onAuthStateChanged, getUserRoles, signInWithGoogle, signOutWithGoogle } from '@/libs/firebase/auth';
 import { User } from 'firebase/auth';
 import { createSession, removeSession } from '@/actions/auth-actions';
-import Index from '@/components';
 import IndexHome from '@/components/home/indexhome';
 import HeaderHome from '@/components/home/headerhome';
 import FooterMobileHome from '@/components/Mobile/footerhome';
 
 interface HomePageProps {
-    session?: string | null;  // session is now optional with a default value of null
+    session?: string | null;
 }
 
-export function HomePage({ session = null }: HomePageProps) {
+const HomePages: React.FC<HomePageProps> = ({ session = null }) => {
     const [user, setUser] = useState<User | null>(null);
     const userSessionId = useUserSession(session);
     const [isAdmin, setIsAdmin] = useState(false);
 
     useEffect(() => {
-      const unsubscribe = onAuthStateChanged(async (authUser) => {
-          setUser(authUser);
-          if (authUser) {
-              const roles = await getUserRoles(authUser.uid);
-              if (Array.isArray(roles)) {
-                  setIsAdmin(roles.includes('admin'));
-              }
-          }
-      });
-  
-      return () => unsubscribe();
-  }, []);
+        const unsubscribe = onAuthStateChanged(async (authUser) => {
+            setUser(authUser);
+            if (authUser) {
+                const roles = await getUserRoles(authUser.uid);
+                if (Array.isArray(roles)) {
+                    setIsAdmin(roles.includes('admin'));
+                }
+            }
+        });
+
+        return () => unsubscribe();
+    }, []);
 
     const handleSignIn = async () => {
         const userUid = await signInWithGoogle();
@@ -51,9 +50,7 @@ export function HomePage({ session = null }: HomePageProps) {
         <main className="relative min-h-screen">
             <HeaderHome />
             <IndexHome session={null} /> 
-            <FooterMobileHome></FooterMobileHome>
+            <FooterMobileHome />
         </main>
     );
-}
-
-export default HomePage;
+};
