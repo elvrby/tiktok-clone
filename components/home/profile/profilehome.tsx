9269efc08beb82e4ef95891773c49651d750a273
elvrby <alfarabi1074@gmail.com>
2024-08-30 19:21:30 +0700

    .

--- components/home/profile/profilehome.tsx
+++ components/home/profile/profilehome.tsx
@@ -38,6 +38,7 @@ const ProfilePageCom: React.FC = () => {
     const [showAlert, setShowAlert] = useState(false);
     const [alertMessage, setAlertMessage] = useState('');
 
+    // Database
     useEffect(() => {
         const fetchAndCheckUserData = async () => {
             try {
@@ -110,7 +111,7 @@ const ProfilePageCom: React.FC = () => {
     }, [router]);
     
     
-
+    // Button Disable
     useEffect(() => {
         const unsubscribe = onauthoriginal(firebaseAuth, async (authUser) => {
             if (!authUser) {
@@ -187,6 +188,7 @@ const ProfilePageCom: React.FC = () => {
         }, 2000);
     };
 
+    // Underline
     const updateUnderline = (buttonRef: React.RefObject<HTMLButtonElement>, transition: string) => {
         if (buttonRef.current) {
             const rect = buttonRef.current.getBoundingClientRect();
@@ -273,8 +275,9 @@ const ProfilePageCom: React.FC = () => {
         return () => {
             document.removeEventListener("mouseup", handleContainerMouseLeave);
         };
-    }, []);
+    }, );
 
+    // Loading
     if (loading) {
         return (
             <div className="flex justify-center items-center h-screen bg-black z-100000000000000000">
@@ -288,7 +291,8 @@ const ProfilePageCom: React.FC = () => {
 
     return (
         
-        <main className="w-full items-center justify-center flex flex-col ">
+        <main className="w-full items-center  flex flex-col ">
+            {/*     Alert    */}
             <div className='relative top-0 w-96 items-center justify-center z-9999999999999999999999'>
                 {showAlert && (
                     <AlertSukses
@@ -307,41 +311,30 @@ const ProfilePageCom: React.FC = () => {
         <header className="fixed w-full h-16 items-center flex p-3 md:hidden z-10">
                 <div className="w-full flex items-center justify-between p-2">
                     <div>
-                        <div>
-                            <svg width="26" height="26" viewBox="0 0 28 28" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <path d="M26.4354 16.2789C26.7747 16.2789 27.0501 16.0036 27.0501 15.6642C27.0501 15.3249 26.7747 15.0496 26.4354 15.0496H22.9163V11.4822H26.4351C26.7744 11.4822 27.0497 11.2068 27.0497 10.8675C27.0497 10.5282 26.7744 10.2528 26.4351 10.2528H22.3016C21.9623 10.2528 21.6869 10.5282 21.6869 10.8675V20.461C21.6869 20.8003 21.9623 21.0756 22.3016 21.0756H26.4351C26.7744 21.0756 27.0497 20.8003 27.0497 20.461C27.0497 20.1217 26.7744 19.8463 26.4351 19.8463H22.9163V16.2789H26.4354Z" fill="white"/>
-                                <path d="M8.25537 10.8675V20.461C8.25537 20.8003 8.53075 21.0756 8.87005 21.0756C9.20936 21.0756 9.48474 20.8003 9.48474 20.461V10.8675C9.48474 10.5282 9.20936 10.2528 8.87005 10.2528C8.53075 10.2528 8.25537 10.5279 8.25537 10.8675Z" fill="white"/>
-                                <path d="M6.19063 20.2237C6.19063 19.8844 5.91525 19.609 5.57594 19.609H2.14783V11.1048C2.14783 10.7655 1.87245 10.4901 1.53314 10.4901C1.19384 10.4901 0.918457 10.7655 0.918457 11.1048V20.2234C0.918457 20.5627 1.19384 20.8381 1.53314 20.8381H5.57564C5.91525 20.8384 6.19063 20.563 6.19063 20.2237Z" fill="white"/>
-                                <path d="M18.5517 10.6613L15.7155 18.628L12.8794 10.6613C12.7656 10.3414 12.4134 10.1748 12.0941 10.2885C11.7741 10.4025 11.6076 10.7541 11.7213 11.0738L15.1368 20.6672C15.2238 20.9121 15.4558 21.0756 15.7158 21.0756C15.9758 21.0756 16.2079 20.9121 16.2949 20.6672L19.7098 11.0738C19.8238 10.7538 19.6566 10.4022 19.337 10.2885C19.017 10.1748 18.6651 10.3414 18.5517 10.6613Z" fill="white"/>
-                                <path d="M2.40877 8.38973C2.74439 8.43398 3.05388 8.19641 3.09783 7.85987C3.32988 6.0828 4.85645 4.74279 6.64857 4.74279H21.3202C23.1124 4.74279 24.6389 6.0828 24.871 7.85956C24.9116 8.16905 25.1756 8.39464 25.4795 8.39464C25.5063 8.39464 25.533 8.39311 25.5601 8.38942C25.8969 8.34547 26.1339 8.0369 26.0902 7.70035C25.7786 5.31322 23.728 3.5131 21.3202 3.5131H15.5517L18.0274 1.21878C18.2766 0.987967 18.2911 0.599177 18.0602 0.350229C17.8291 0.100973 17.44 0.086835 17.1917 0.317343L13.9843 3.29028L10.7768 0.31765C10.5276 0.0865276 10.1391 0.101895 9.90826 0.350536C9.67745 0.599484 9.6922 0.988274 9.94115 1.21909L12.4168 3.51341H6.64827C4.24084 3.51341 2.19055 5.31352 1.8786 7.70035C1.83496 8.03751 2.07192 8.34608 2.40877 8.38973Z" fill="white"/>
-                                <path d="M25.5597 22.9385C25.226 22.8939 24.9146 23.1318 24.8707 23.4686C24.6386 25.2454 23.1124 26.5854 21.3199 26.5854H6.64827C4.85615 26.5854 3.32988 25.2454 3.09783 23.4683C3.05388 23.1315 2.74408 22.896 2.40877 22.9382C2.07192 22.9824 1.83496 23.2907 1.8786 23.6272C2.19055 26.0147 4.24115 27.8148 6.64827 27.8148H21.3199C23.7277 27.8148 25.7783 26.0147 26.0899 23.6275C26.1336 23.291 25.8966 22.9824 25.5597 22.9385Z" fill="white"/>
-                            </svg>
+                        <div className="w-6">
+                        <Image src="/TiktokCoin.png" alt="" width={500} height={300} />
                         </div>
                     </div>
 
-                    <div className="flex justify-between w-56">
-                        <div>
-                            <h1 className="text-[#DADADA]">Friends</h1>
-                        </div>
-                        <div>
-                            <h1 className="text-[#DADADA]">Following</h1>
-                        </div>
-                        <div className="flex flex-col items-center">
-                            <h1 className="font-bold">For You</h1>
-                            {/* Line */}
-                            <div className="w-8 bg-white h-[2px]"></div>
+                    <div className="flex justify-center w-56">
+                        <div className="flex items-center">
+                            <h2 className="font-bold pr-1">{username ? username : 'user'}</h2>
+                            <svg width="10" height="14" viewBox="0 0 23 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M1 1L11.5 13L22 1" stroke="white" strokeWidth={3}/>
+                            </svg>
                         </div>
                     </div>
 
                     
 
                     <div>
-                        <div>
-                            <svg width="20" height="30" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M21.9141 18.2812C23.0859 16.4375 23.7734 14.25 23.7734 11.8984C23.7734 5.32813 18.4531 0 11.8906 0C5.32031 0 0 5.32813 0 11.8984C0 18.4688 5.32031 23.7969 11.8828 23.7969C14.2656 23.7969 16.4844 23.0938 18.3438 21.8906C18.6557 21.6736 19.0784 21.7112 19.3471 21.98L26.027 28.6598C26.7652 29.3981 27.9638 29.3928 28.6955 28.648C29.4179 27.9128 29.413 26.7327 28.6845 26.0036L22.0029 19.3159C21.7258 19.0385 21.6883 18.6019 21.9141 18.2812ZM18.5469 5.25C20.3203 7.02344 21.2969 9.38281 21.2969 11.8906C21.2969 14.3984 20.3203 16.7578 18.5469 18.5312C16.7734 20.3047 14.4141 21.2812 11.9062 21.2812C9.39844 21.2812 7.03906 20.3047 5.26562 18.5312C3.49219 16.7578 2.51562 14.3984 2.51562 11.8906C2.51562 9.38281 3.49219 7.02344 5.26562 5.25C7.03906 3.47656 9.39844 2.5 11.9062 2.5C14.4141 2.5 16.7734 3.47656 18.5469 5.25Z" fill="#DADADA"/>
+                        <button>
+                            <svg className="w-4" viewBox="0 0 13 8" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M12.5149 0.936037H0.485074C0.356425 0.936037 0.233044 0.886728 0.142075 0.798958C0.0511059 0.711187 0 0.592145 0 0.468019C0 0.343892 0.0511059 0.22485 0.142075 0.137079C0.233044 0.0493086 0.356425 0 0.485074 0H12.5149C12.6436 0 12.767 0.0493086 12.8579 0.137079C12.9489 0.22485 13 0.343892 13 0.468019C13 0.592145 12.9489 0.711187 12.8579 0.798958C12.767 0.886728 12.6436 0.936037 12.5149 0.936037Z" fill="white"/>
+                                <path d="M12.5149 4.46802H0.485075C0.356425 4.46802 0.233044 4.41871 0.142075 4.33094C0.051106 4.24317 6.46176e-08 4.12413 6.46176e-08 4C6.46176e-08 3.87587 0.051106 3.75683 0.142075 3.66906C0.233044 3.58129 0.356425 3.53198 0.485075 3.53198H12.5149C12.6436 3.53198 12.767 3.58129 12.8579 3.66906C12.9489 3.75683 13 3.87587 13 4C13 4.12413 12.9489 4.24317 12.8579 4.33094C12.767 4.41871 12.6436 4.46802 12.5149 4.46802Z" fill="white"/>
+                                <path d="M12.5149 8H0.485075C0.356425 8 0.233044 7.95069 0.142075 7.86292C0.051106 7.77515 6.46176e-08 7.65611 6.46176e-08 7.53198C6.46176e-08 7.40785 0.051106 7.28881 0.142075 7.20104C0.233044 7.11327 0.356425 7.06396 0.485075 7.06396H12.5149C12.6436 7.06396 12.767 7.11327 12.8579 7.20104C12.9489 7.28881 13 7.40785 13 7.53198C13 7.65611 12.9489 7.77515 12.8579 7.86292C12.767 7.95069 12.6436 8 12.5149 8Z" fill="white"/>
                             </svg>
-
-                        </div>
+                        </button>
                     </div>
                 </div>
         </header>
@@ -619,11 +612,13 @@ const ProfilePageCom: React.FC = () => {
                                     onUsernameUpdated={handleUsernameUpdated} onNameUpdated={handleNameUpdated} onBioUpdated={handleBioUpdated}/>
                                 )}
                                 </div>
+                                {canEditProfile && (
                                 <button className="bg-[#1E1E1E] w-11  p-3 rounded-lg mr-3">
                                     <svg width="21" height="22" viewBox="0 0 21 22" fill="none" xmlns="http://www.w3.org/2000/svg">
                                         <path d="M19.7352 8.7347H18.9031C18.2243 8.7347 17.655 8.12184 17.655 7.39443C17.655 7.02786 17.8028 6.69565 18.0655 6.44936L18.602 5.89951C19.1331 5.34965 19.1331 4.4504 18.602 3.90055L17.3812 2.63473C17.1404 2.38271 16.7845 2.23379 16.4232 2.23379C16.0619 2.23379 15.7116 2.38271 15.4652 2.63473L14.9506 3.17313C14.7043 3.45952 14.3758 3.61416 14.02 3.61416C13.3192 3.61416 12.7335 3.01848 12.7335 2.31398V1.43765C12.7335 0.66441 12.1368 0 11.3922 0H9.72802C8.98351 0 8.39227 0.658682 8.39227 1.43765V2.30825C8.39227 3.01276 7.80651 3.60844 7.10578 3.60844C6.75542 3.60844 6.43243 3.45379 6.19703 3.18459L5.66601 2.63473C5.42514 2.37699 5.0693 2.23379 4.70799 2.23379C4.34668 2.23379 3.99632 2.38271 3.74997 2.63473L2.51823 3.89482C1.99268 4.44468 1.99268 5.34392 2.51823 5.88805L3.03282 6.42645C3.30654 6.6842 3.45983 7.02786 3.45983 7.39443C3.45983 8.12757 2.89049 8.7347 2.21166 8.7347H1.37955C0.629557 8.7347 0 9.34757 0 10.1265V10.9971V11.8677C0 12.641 0.629557 13.2596 1.37955 13.2596H2.21166C2.89049 13.2596 3.45983 13.8724 3.45983 14.5998C3.45983 14.9664 3.30654 15.3101 3.03282 15.5678L2.51823 16.1005C1.99268 16.6504 1.99268 17.5496 2.51823 18.0937L3.73902 19.3653C3.9799 19.623 4.33573 19.7662 4.69704 19.7662C5.05835 19.7662 5.40872 19.6173 5.65506 19.3653L6.18608 18.8154C6.41601 18.5462 6.74447 18.3916 7.09483 18.3916C7.79556 18.3916 8.38132 18.9872 8.38132 19.6917V20.5624C8.38132 21.3356 8.97256 22 9.72255 22H11.3868C12.1313 22 12.7225 21.3413 12.7225 20.5624V19.6917C12.7225 18.9872 13.3083 18.3916 14.009 18.3916C14.3594 18.3916 14.6878 18.5519 14.9397 18.8326L15.4543 19.371C15.7006 19.623 16.051 19.7719 16.4123 19.7719C16.7736 19.7719 17.124 19.623 17.3703 19.371L18.5911 18.0995C19.1166 17.5496 19.1166 16.6504 18.5911 16.1005L18.0546 15.5506C17.7918 15.3043 17.644 14.9664 17.644 14.6056C17.644 13.8724 18.2134 13.2653 18.8922 13.2653H19.7243C20.4688 13.2653 20.9998 12.6524 20.9998 11.8735V10.9971V10.1265C21.0108 9.34757 20.4798 8.7347 19.7352 8.7347ZM14.9342 10.9971C14.9342 13.523 12.9798 15.5793 10.5547 15.5793C8.1295 15.5793 6.17513 13.523 6.17513 10.9971C6.17513 8.47123 8.1295 6.415 10.5547 6.415C12.9798 6.415 14.9342 8.47123 14.9342 10.9971Z" fill="#DADADA"/>
                                     </svg>
                                 </button>
+                                )}
                                 <button className="bg-[#1E1E1E] w-11  p-3 rounded-lg">
                                     <svg width="21" height="24" viewBox="0 0 29 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                                         <path d="M8.49569 17.3381H15.974L16.0183 23.015L29 11.9696L15.9303 0V5.06086C15.9303 5.06086 0 3.3005 0 22.0033L3.21667 19.2755C4.69171 18.0249 6.56246 17.3387 8.49621 17.3387L8.49569 17.3381Z" stroke="white" strokeWidth={"2"}/>
@@ -706,8 +701,43 @@ const ProfilePageCom: React.FC = () => {
 
 
             {/* Main Konten Mobile */}
-            <div className="relative w-full h-full text-center flex items-center justify-center md:hidden">
+            <div className="relative w-full h-full overflow-y-auto text-center flex justify-center md:hidden">
+                <div className="w-full pt-20">
+                    <div className="w-full flex flex-col items-center">
+                        <div className="w-28 h-28 bg-white rounded-full mb-4">
 
+                        </div>
+                        <h2>@{name ? name : 'user'}</h2>
+                    </div>
+                    <div className="flex w-full justify-center mt-4">
+                        <div className="mr-2 flex flex-col w-20">
+                            <span>{followingCount}</span>
+                            <span className="ml-1 text-[#DADADA] text-sm">Following</span>
+                        </div>
+                        <div className="mr-2 flex flex-col w-20">
+                            <span>{followersCount}</span>
+                            <span className="ml-1 text-[#DADADA] text-sm">Followers</span>
+                        </div>
+                        <div className="mr-2 flex flex-col w-16 justify-center">
+                            <span>{likesCount}</span>
+                            <span className="ml-1 text-[#DADADA] text-sm">Likes</span>
+                        </div>
+                    </div>
+                    {canEditProfile && (
+                    <div className="w-full flex items-center justify-center mt-4">
+                        <button className="bg-[#4A4A4A] w-auto p-2 pl-5 pr-5 rounded-md text-sm mr-1">Edit Profile</button>
+                        <button className="bg-[#4A4A4A] w-auto p-2 pl-5 pr-5 rounded-md text-sm mr-1">Share Profile</button>
+                        <button className="bg-[#4A4A4A] w-5 p-2 pl-5 pr-5 rounded-md text-sm items-center justify-center flex flex-col">
+                            <svg className="w-4" viewBox="0 0 19 28" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M1.00785 27C1.00785 27 0.234125 17.168 14.048 17.168M15 15V24.5M11 19.5H19M16.9353 6.65197C16.9353 9.77347 14.4048 12.3039 11.2833 12.3039C8.1618 12.3039 5.63133 9.77347 5.63133 6.65197C5.63133 3.53047 8.1618 1 11.2833 1C14.4048 1 16.9353 3.53047 16.9353 6.65197Z" stroke="white" stroke-miterlimit="10" strokeWidth={2}/>
+                            </svg>
+                        </button>
+                    </div>
+                    )}
+                    <div className="w-full flex text-[#DADADA] text-sm mt-3 text-start items-center justify-center">
+                        <span>{bio ? bio : 'No bio yet'}</span>
+                    </div>
+                </div>
                 
             </div>
 
